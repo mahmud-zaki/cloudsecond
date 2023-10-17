ce193f32f33b43af38516f84bc228df312cd07fa
mahmud-zaki <mahmudzaki.s@gmail.com>
2023-10-17 12:28:01 +0700

    add some function

--- cloud_s/urls.py
+++ cloud_s/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='cloud_s'),
-    path('add-cloud_s', views.add_cloud_s, name='add-cloud_s')
+    path("", views.index, name="cloud_s"),
+    path("add-cloud_s", views.add_cloud_s, name="add-cloud_s"),
+    path("cloud-edit/<int:id>", views.cloud_edit, name="cloud-edit"),
 ]
--- cloud_s/views.py
+++ cloud_s/views.py
@@ -1,9 +1,84 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Category, Cloud_s
+from django.contrib import messages
+
 # Create your views here.
 
-@login_required(login_url='/authentication/login')
+
+@login_required(login_url="/authentication/login")
 def index(request):
-    return render(request, 'cloud_s/index.html')
+    categories = Category.objects.all()
+    cloud_s = Cloud_s.objects.filter(owner=request.user)
+
+    context = {"cloud_s": cloud_s}
+    return render(request, "cloud_s/index.html", context)
+
+
 def add_cloud_s(request):
-    return render(request, 'cloud_s/add_cloud_s.html')
+    categories = Category.objects.all()
+    context = {"categories": categories, "values": request.POST}
+    if request.method == "GET":
+        return render(request, "cloud_s/add_cloud_s.html", context)
+
+    if request.method == "POST":
+        amount = request.POST["amount"]
+        description = request.POST["description"]
+        date = request.POST["cloud_date"]
+        category = request.POST["category"]
+
+        if not amount:
+            messages.error(request, "amount is required")
+            return render(request, "cloud_s/add_cloud_s.html", context)
+
+        if not description:
+            messages.error(request, "Description is required")
+            return render(request, "cloud_s/add_cloud_s.html", context)
+
+    Cloud_s.objects.create(
+        owner=request.user,
+        amount=amount,
+        date=date,
+        category=category,
+        description=description,
+    )
+    messages.success(request, "Cloud saved succesfully")
+
+    return redirect("cloud_s")
+
+
+def cloud_edit(request, id):
+    cloud_s = Cloud_s.objects.get(pk=id)
+    categories = Category.objects.all()
+    context = {
+        "cloud_s": cloud_s,
+        "values": cloud_s,
+        "categories": categories,
+    }
+    if request.method == "GET":
+        return render(request, "cloud_s/cloud-edit.html", context)
+    if request.method == "POST":
+        amount = request.POST["amount"]
+
+        if not amount:
+            messages.error(request, "amount is required")
+            return render(request, "cloud_s/cloud-edit.html", context)
+
+        description = request.POST["description"]
+        date = request.POST["cloud_date"]
+        category = request.POST["category"]
+
+        if not description:
+            messages.error(request, "Description is required")
+            return render(request, "cloud_s/cloud-edit.html", context)
+
+    cloud_s.owner = request.user
+    cloud_s.amount = amount
+    cloud_s.date = date
+    cloud_s.category = category
+    cloud_s.description = description
+
+    cloud_s.save()
+    messages.success(request, "Cloud updated succesfully")
+
+    return redirect("cloud_s")
